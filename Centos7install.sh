116b819a8bcf040a9d2ebe0c96cdb7f5f91c8ff2
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2021-03-28 12:55:55 -0500

    Update Centos7install.sh

--- Centos7install.sh
+++ Centos7install.sh
@@ -21,7 +21,27 @@ yum uqpdate -y
 # REMI Repository initialization
 yum install -y epel-release
 wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-rpm -Uvh remi-release-7.rpm
+rpm -Uvh remi-release-7.rpmecho "Name of databases seperated by spaces to restore?"
+read -p 'databases: ' dbases
+
+DIR="/mnt/backup/sql/${dbases}"
+TODAY=$(date +"%a")
+DB_FILE=${TODAY}.sql.gz
+
+
+for dbase in $dbases
+ do
+  if [ ! -f "${DIR}/${TODAY}.sql" ] ; then
+   gunzip -f "${DIR}/${DB_FILE}"
+   mysql --user=admin -p ${dbase} < $DIR/$TODAY.sql
+   echo "Backup complete for dbase ${dbase} ..."
+   echo "Returning file for dbase ${dbase} back to compressed state ..."
+   gzip ${DIR}/${TODAY}.sql
+  else
+    echo "- ${TODAY}.sql already exists for dbase ${dbase} and will be gzipped ..."
+    gzip ${DIR}/${TODAY}.sql
+  fi
+done
 yum update -y
 
 # Apache web server and security packages
@@ -138,27 +158,31 @@ _EOF_
 
 # Gunzip latest database backup sql.gz file for each databse and restore the database
 
-echo "Name of databases seperated by spaces to restore?"
-read -p 'databases: ' dbases
+echo "Name of databases to restore seperated by spaces ONLY?"
+read -p "database: " dbases
 
-DIR="/mnt/backup/sql/${dbases}"
+
+DIR="/mnt/backup/sql"
 TODAY=$(date +"%a")
 DB_FILE=${TODAY}.sql.gz
 
 
-for dbase in $dbases
+for dbase in ${dbases[@]}
  do
-  if [ ! -f "${DIR}/${TODAY}.sql" ] ; then
-   gunzip -f "${DIR}/${DB_FILE}"
-   mysql --user=admin -p ${dbase} < $DIR/$TODAY.sql
-   echo "Backup complete for dbase ${dbase} ..."
+  if [ ! -f "${DIR}/${dbase}/${TODAY}.sql" ] ; then
+   gunzip -f "${DIR}/${dbase}/${DB_FILE}"
+   mysql --user=admin -p ${dbase} < $DIR/${dbase}/$TODAY.sql
+   echo "**** Success ****"
+   echo "Restore complete for dbase ${dbase} ..."
    echo "Returning file for dbase ${dbase} back to compressed state ..."
-   gzip ${DIR}/${TODAY}.sql
-  else
+   ehco "*****************"
+   gzip ${DIR}/${dbase}/${TODAY}.sql
+else
     echo "- ${TODAY}.sql already exists for dbase ${dbase} and will be gzipped ..."
-    gzip ${DIR}/${TODAY}.sql
-  fi
+    gzip ${DIR}/${dbase}/${TODAY}.sql
+fi
 done
+
 
 echo "Securing SQL installation"
 
